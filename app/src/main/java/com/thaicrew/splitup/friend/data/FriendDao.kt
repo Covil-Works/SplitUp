8b2a4b6340adf86e49098fe0cc9812e98b4d3413
artuenric <artuenrick@gmail.com>
2025-07-12 21:49:06 -0300

    feat: pacote friend criado

---
+++ app/src/main/java/com/thaicrew/splitup/friend/data/FriendDao.kt
@@ -0,0 +1,22 @@
+package com.thaicrew.splitup.friend.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FriendDao {
+    @Insert
+    suspend fun insert(friend: FriendEntity)
+
+    @Update
+    suspend fun update(friend: FriendEntity)
+
+    @Query("SELECT * from friend WHERE friend_is_active = 1 ORDER BY friend_name ASC")
+    suspend fun getActiveFriends(): Flow<List<FriendEntity>>
+
+    @Query("SELECT * FROM friend WHERE friend_name = :name LIMIT 1")
+    suspend fun findByName(name: String): FriendEntity?
+}
---
+++ app/src/main/java/com/thaicrew/splitup/friend/domain/Friend.kt
@@ -0,0 +1,7 @@
+package com.thaicrew.splitup.friend.domain
+
+data class Friend(
+    val id: Int,
+    val name: String,
+    val isActive: Boolean
+)

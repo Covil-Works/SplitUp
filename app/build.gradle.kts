cf83e155303d1efe8cb4f93581d93d3e98913de6
Arthur Henrique <artuenrick@gmail.com>
2025-07-15 18:30:48 -0300

    chore: add /build no gitignore e versões atualizadas
    versões do ksp e room e kotlin atualizadas para compatibilidade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.kotlin.ksp) // id("com.google.devtools.ksp") version "2.1.21-2.0.1"
 }
 
 android {
@@ -60,4 +60,8 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
 }

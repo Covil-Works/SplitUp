292437490f174d491139c143926f8900cadbad9f
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2026-04-30 10:30:31 -0300

    feat(color): redefine as cores da interface

--- app/src/main/java/com/thaicrew/splitup/ui/theme/Color.kt
+++ app/src/main/java/com/thaicrew/splitup/ui/theme/Color.kt
@@ -3,15 +3,15 @@ package com.thaicrew.splitup.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val PurpleAction = Color(0xFF5B4CCE)
-val PurpleLight = Color(0xFFDDDAF8)
+val PurpleLight = Color(0xFFE5E4F7)
 
-val DarkBackground = Color(0xFF07090C)
+val DarkBackground = Color(0xFF1A1E2D)
 val SurfaceBlueGrey = Color(0xFF1E2034)
-val SurfaceCard = Color(0xFF171A2C)
+val SurfaceCard = Color(0xFF2B2E44)
 val SurfaceCardElevated = Color(0xFF303654)
-val BarBackground = Color(0xFF262B45)
+val BarBackground = Color(0xFF303350)
 
-val LightGreyText = Color(0xFFF4F6FF)
+val LightGreyText = Color(0xFFFAFBFF)
 val MediumContrastText = Color(0xFFB9C1E2)
 val SoftOutline = Color(0xFF3B4160)
 val DestructiveAccent = Color(0xFFE67282)

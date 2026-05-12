a364049f7a18b1905bb25aed52807ce20685aff7
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2026-05-12 15:44:38 -0300

    chore(version): add version 1.0.2 on build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.covildev.splitup"
         minSdk = 32
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
